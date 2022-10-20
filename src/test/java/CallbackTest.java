987ed63dc3349ec09d2a6c322f2ed54977253515
ЕленаПрекрасная <timoninae079@gmail.com>
2022-10-20 17:13:00 +0300

    '20'

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,26 +1,34 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CallbackTest {
-    private WebDriver driver;
+    private WebDriver driver = new ChromeDriver();
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webDriver.chrome.driver", "driver/chromedriver.exe");
+
+        System.setProperty("WebDriver.chrome.driver", "driver/Chromedriver.exe");
     }
 
     @BeforeEach
     void setUp() {
+
         driver = new ChromeDriver();
     }
 
     @AfterEach
-
-    void tearsDow(){
+    void tearsDow() {
         driver.quit();
-        driver=null;
+        driver = null;
+    }
+
+    @Test
+    void Test() {
+        driver.get("http://lokalhost:9999");
     }
 }
+
