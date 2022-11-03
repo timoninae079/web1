bc88bdc4b2316cbcfb706cea6094338788d093ad
ЕленаПрекрасная <timoninae079@gmail.com>
2022-11-03 21:39:10 +0300

    '1'

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,36 +1,33 @@
-import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.*;
 
-class CallbackTest {
+public class CallbackTest {
+    private WebDriver driver;
+
     @BeforeAll
     static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
-    void tearsDow() {
-    }
-
-    @Test
-    void shouldTestSomething() {
-
-        open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена"));
-        sleep(10000);
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
-
